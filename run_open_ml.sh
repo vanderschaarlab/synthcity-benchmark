d112c6a8f04aa0cbd2305698243af45560f8e61f
Zhaozhi <qianzhaozhi@yahoo.com>
2023-03-21 10:40:42 +0000

    get OpenML datasets

---
+++ run_open_ml.sh
@@ -0,0 +1,4 @@
+python -u open_ml.py --data_type=num --task_type=regression &
+python -u open_ml.py --data_type=num --task_type=classification &
+python -u open_ml.py --data_type=cat --task_type=regression &
+python -u open_ml.py --data_type=cat --task_type=classification &
